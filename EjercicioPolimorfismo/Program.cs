322d6a46339298597b8de5eb24869eba67f2155f
JoeDB993 <131511247+JoeDB993@users.noreply.github.com>
2024-08-07 13:48:49 -0600

    Instancia de las clases creada.

--- EjercicioPolimorfismo/Program.cs
+++ EjercicioPolimorfismo/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace EjercicioPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba el nombre del perro: ");
+            Animal perro = new Perro(Console.ReadLine());
+            Console.WriteLine("Escriba el nombre del Gato: ");
+            Animal gato = new Gato(Console.ReadLine());
+            Console.WriteLine("Escriba el nombre del pájaro: ");
+            Animal pajaro = new Pajaro(Console.ReadLine());
+
+            Animal[] animales = { perro, gato, pajaro };
+
+            foreach (Animal animal in animales)
+            {
+                animal.HacerSonido();
+                animal.Moverse();
+                Console.WriteLine();
+            }
+        }
+    }
+}
