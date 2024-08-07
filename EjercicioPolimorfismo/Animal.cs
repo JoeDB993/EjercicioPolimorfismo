ec9bc9ffde9ebd352ab73b38091aa9ae3af52502
JoeDB993 <131511247+JoeDB993@users.noreply.github.com>
2024-08-07 13:39:31 -0600

    Creación de clase Animal

---
+++ EjercicioPolimorfismo/Animal.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace EjercicioPolimorfismo
+{
+    abstract class Animal
+    {
+        public string Nombre { get; set; }
+
+        public Animal(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public abstract void HacerSonido();
+
+        public virtual void Moverse()
+        {
+            Console.WriteLine($"{Nombre} se está moviendo.");
+        }
+    }
+}
