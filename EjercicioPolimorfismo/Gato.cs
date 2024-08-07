b4e9a600104478941eafa5ef35989aafabd76d08
JoeDB993 <131511247+JoeDB993@users.noreply.github.com>
2024-08-07 13:45:22 -0600

    Clase pájaro agregada.

---
+++ EjercicioPolimorfismo/Gato.cs
@@ -0,0 +1,20 @@
+﻿namespace EjercicioPolimorfismo
+{
+    class Gato : Animal
+    {
+        public Gato(string nombre) : base(nombre)
+        {
+        }
+
+        public override void HacerSonido()
+        {
+            Console.WriteLine($"{Nombre} dice: ¡Miau!");
+        }
+
+        public override void Moverse()
+        {
+            Console.WriteLine($"{Nombre} está caminando sigilosamente.");
+        }
+    }
+
+}
---
+++ EjercicioPolimorfismo/Pajaro.cs
@@ -0,0 +1,19 @@
+﻿namespace EjercicioPolimorfismo
+{
+    class Pajaro : Animal
+    {
+        public Pajaro(string nombre) : base(nombre)
+        {
+        }
+
+        public override void HacerSonido()
+        {
+            Console.WriteLine($"{Nombre} dice: ¡Pío pío!");
+        }
+
+        public override void Moverse()
+        {
+            Console.WriteLine($"{Nombre} está volando.");
+        }
+    }
+}
